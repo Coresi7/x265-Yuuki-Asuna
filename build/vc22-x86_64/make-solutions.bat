a611dad960a42a9835fcafe7471fb932f1005b31
Coresi7 <coresi7@qq.com>
2022-07-25 17:48:51 +0800

    update

--- build/vc22-x86_64/make-solutions.bat
+++ build/vc22-x86_64/make-solutions.bat
@@ -3,4 +3,7 @@
 :: run this batch file to create a Visual Studio solution file for this project.
 :: See the cmake documentation for other generator targets
 ::
+if not exist CMakeCache.txt (
+    copy ..\CMakeDemo\CMakeCache.txt %~dp0
+)
 cmake -G "Visual Studio 17" ..\..\source && cmake-gui ..\..\source
